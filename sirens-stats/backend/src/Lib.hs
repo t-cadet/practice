f639650050f9704af90413be4cfd1ca7e3157ef5
tristanCadet <tristan.cadet@insa-lyon.fr>
2020-07-23 18:32:12 +0200

    NEW: sirenFromInt (& isLuhn) + tests

--- sirens-stats/backend/src/Lib.hs
+++ sirens-stats/backend/src/Lib.hs
@@ -1,11 +1,38 @@
 module Lib
     ( countUniqueAndNonUnique
     , cuanuNaive
+    , digits
+    , toLuhnForm
+    , isLuhn
+    , sirenFromInt
     ) where
 
 import Data.List
 import Data.Map (Map)
 import qualified Data.Map as Map
+
+newtype Siren = Siren Integer 
+    deriving (Eq, Show)
+
+sirenFromInt :: Integer -> Maybe Siren
+sirenFromInt x 
+    | 0 <= x && x < 10^9 && isLuhn x = Just (Siren x)
+    | otherwise = Nothing
+
+isLuhn x = mod (sum $ toLuhnForm x) 10 == 0
+
+toLuhnForm x = helper (reverse $ digits x) []
+    where
+        helper (a:b:xs) acc = helper xs (sumDigit (2*b):a:acc)
+        helper [x] acc = x:acc
+        helper [] acc = acc
+        sumDigit x = if x > 9 then x-9 else x 
+
+digits x = helper x []
+    where
+        helper 0 acc = acc
+        helper x acc = helper q (r:acc)
+            where (q, r) = quotRem x 10
 
 countUniqueAndNonUnique l = helper l Map.empty 0 0
   where 
--- sirens-stats/backend/test/Spec.hs
+++ sirens-stats/backend/test/Spec.hs
@@ -1,17 +1,60 @@
 import Test.HUnit
+import Data.Maybe
 
 import Lib
 
 main = runTestTT tests
 
-tests = test $ (testCuanu cuanuNaive) ++ (testCuanu countUniqueAndNonUnique)
+tests = test $ 
+       (testCuanu cuanuNaive) 
+    ++ (testCuanu countUniqueAndNonUnique)
+    ++ testDigits
+    ++ testIsLuhn
 
 testCuanu cuanu = [ 
-      "" ~: "" ~: (0,0) ~=? (cuanu ([]::[Integer]))
-    , "" ~: "" ~: (1,0) ~=? (cuanu [0])
-    , "" ~: "" ~: (0,1) ~=? (cuanu [0, 0])
-    , "" ~: "" ~: (1,1) ~=? (cuanu [0, 1, 1])
-    , "" ~: "" ~: (0,6) ~=? (cuanu ([0..5]++[0..5]))
-    , "" ~: "" ~: (3,2) ~=? (cuanu [0, 1, 1, 2, 3, 4, 4])
-    , "" ~: "" ~: (1,5) ~=? (cuanu (6:(take 70000 $ cycle [0, 1, 1, 2, 3, 4, 4])))
+      "cuanu" ~: "" ~: (0,0) ~=? (cuanu ([]::[Integer]))
+    , "cuanu" ~: "" ~: (1,0) ~=? (cuanu [0])
+    , "cuanu" ~: "" ~: (0,1) ~=? (cuanu [0, 0])
+    , "cuanu" ~: "" ~: (1,1) ~=? (cuanu [0, 1, 1])
+    , "cuanu" ~: "" ~: (0,6) ~=? (cuanu ([0..5]++[0..5]))
+    , "cuanu" ~: "" ~: (3,2) ~=? (cuanu [0, 1, 1, 2, 3, 4, 4])
+    , "cuanu" ~: "" ~: (1,5) ~=? (cuanu (6:(take 70000 $ cycle [0, 1, 1, 2, 3, 4, 4])))
+    ]
+
+testDigits = [
+      "digits" ~: "" ~: [] ~=? (digits 0)
+    , "digits" ~: "" ~: [1] ~=? (digits 1)
+    , "digits" ~: "" ~: [1, 0] ~=? (digits 10)
+    , "digits" ~: "" ~: [7, 2, 3, 8, 7, 0] ~=? (digits 723870)
+    ]
+
+testToLuhnForm = [
+      "toLuhnForm" ~: "" ~: [0] ~=? (toLuhnForm 0)
+    , "toLuhnForm" ~: "" ~: [1] ~=? (toLuhnForm 1)
+    , "toLuhnForm" ~: "" ~: [2, 0] ~=? (toLuhnForm 10)
+    , "toLuhnForm" ~: "" ~: [5, 2, 6, 8, 5, 0] ~=? (toLuhnForm 723870)
+    , "toLuhnForm" ~: "" ~: [9, 5, 2, 8, 8, 5, 0, 7, 6] ~=? (toLuhnForm 972487086)
+    , "toLuhnForm" ~: "" ~: [9, 4, 7, 8, 8, 5, 0, 7, 6] ~=? (toLuhnForm 927487086)
+    ]
+
+testIsLuhn = [
+      "isLuhn" ~: "" ~: True ~=? (isLuhn 0)
+    , "isLuhn" ~: "" ~: False ~=? (isLuhn 1)
+    , "isLuhn" ~: "" ~: False ~=? (isLuhn 10)
+    , "isLuhn" ~: "" ~: False ~=? (isLuhn 723870)
+    , "isLuhn" ~: "" ~: True ~=? (isLuhn 972487086)
+    , "isLuhn" ~: "" ~: False ~=? (isLuhn 927487086)
+    , "isLuhn" ~: "" ~: True ~=? (isLuhn 97248708600)
+    ]
+
+testSirenFromInt = [
+      "sirenFromInt" ~: "" ~: True ~=? isJust (sirenFromInt 0)
+    , "sirenFromInt" ~: "" ~: Nothing ~=? (sirenFromInt (-1))
+    , "sirenFromInt" ~: "" ~: Nothing ~=? (sirenFromInt (10^9))
+    , "sirenFromInt" ~: "" ~: Nothing ~=? (sirenFromInt 1)
+    , "sirenFromInt" ~: "" ~: Nothing ~=? (sirenFromInt 10)
+    , "sirenFromInt" ~: "" ~: Nothing ~=? (sirenFromInt 723870)
+    , "sirenFromInt" ~: "" ~: True ~=? isJust (sirenFromInt 972487086)
+    , "sirenFromInt" ~: "" ~: Nothing ~=? (sirenFromInt 927487086)
+    , "sirenFromInt" ~: "" ~: Nothing ~=? (sirenFromInt 97248708600)    
     ]
